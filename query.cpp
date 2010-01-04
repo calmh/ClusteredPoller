4ca7e67b16b601024d4902ceafab7a5923d0d737
Jakob Borg <jakob@nym.se>
2010-01-04 13:04:33 +0100

    Queue statistics at end of database writes or beginning of poll.
    Closes #2.

--- query.cpp
+++ query.cpp
@@ -263,28 +263,42 @@ void* poller_thread(void *ptr)
 void* monitor_thread(void *ptr)
 {
 	time_t interval = 0; //(time(NULL) / config.interval + 1) * config.interval;
-	int in_iteration = 0;
+	time_t iteration_completed = 0;
+	time_t in_iteration = 0;
 	while (1) {
 		sleep(1);
 		if (active_threads == 0 && in_iteration) {
 			stat_iterations++;
-			if (verbosity >= 1) {
+			if (verbosity > 0) {
 				pthread_mutex_lock(&db_list_lock);
 				unsigned qd = queries.size();
 				pthread_mutex_unlock(&db_list_lock);
 				pthread_mutex_lock(&cerr_lock);
-				cerr << "Monitor sees everyone is complete. Elapsed time for this iteration #" << stat_iterations << " was " << time(NULL) - in_iteration << "s." << endl;
-				cerr << "Time until next iteration is " << interval - time(NULL) << "s." << endl;
+				cerr << "Iteration complete, elapsed time for #" << stat_iterations << " was " << time(NULL) - in_iteration << " s." << endl;
+				cerr << "Time until next iteration is " << interval - time(NULL) << " s." << endl;
 				cerr << "  Rows inserted: " << stat_inserts << endl;
 				cerr << "  Queries queued: " << stat_queries << endl;
 				cerr << "  Max queue depth: " << query_queue_depth << endl;
 				cerr << "  Remaining queue: " << qd << endl;
 				pthread_mutex_unlock(&cerr_lock);
+				if (qd > 0)
+					iteration_completed = time(NULL);
 			}
 			stat_inserts = 0;
 			stat_queries = 0;
 			in_iteration = 0;
 			query_queue_depth = 0;
+		}
+
+		if (active_threads == 0 && iteration_completed) {
+			pthread_mutex_lock(&db_list_lock);
+			unsigned qd = queries.size();
+			pthread_mutex_unlock(&db_list_lock);
+			if (qd == 0) {
+				if (verbosity > 0)
+					cerr << "  Queue zero " << time(NULL) - iteration_completed << " s after poll completion." << endl;
+				iteration_completed = 0;
+			}
 		}
 
 		pthread_mutex_lock(&global_lock);
@@ -295,6 +309,12 @@ void* monitor_thread(void *ptr)
 				pthread_mutex_lock(&cerr_lock);
 				cerr << "Monitor signals wakeup." << endl;
 				pthread_mutex_unlock(&cerr_lock);
+				pthread_mutex_lock(&db_list_lock);
+				unsigned qd = queries.size();
+				pthread_mutex_unlock(&db_list_lock);
+				if (qd > 0 && verbosity > 0)
+					cerr << "  Queue at depth " << qd << " at poll start." << endl;
+				iteration_completed = 0;
 			}
 			pthread_cond_broadcast(&global_cond);
 		}
