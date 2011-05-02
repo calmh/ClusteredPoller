7e9e703d24937606d1b7c31715685b01ccf363ed
Jakob Borg <jakob@nym.se>
2011-05-02 19:02:07 +0200

    Refactor monitoring loop.

--- src/monitor.c
+++ src/monitor.c
@@ -16,6 +16,9 @@
 #include "rtgtargets.h"
 #include <unistd.h>
 
+void start_iteration(struct rtgtargets *targets);
+void end_iteration(curms_t interval, curms_t this_iteration);
+
 void *monitor_run(void *ptr)
 {
         struct mt_context *thread_context = (struct mt_context *) ptr;
@@ -25,72 +28,87 @@ void *monitor_run(void *ptr)
         struct rtgconf *config = monitor_context->config;
         unsigned poll_interval = config->interval;
 
-        int in_iteration = 0;
-        curms_t interval = 0;
-        curms_t iteration_started = 0;
-        curms_t now = 0;
+        curms_t next_iteration = 0;
+        curms_t this_iteration = 0;
 
         struct timespec loopdelay = { 0, 250 * 1000 * 1000 };
         nanosleep(&loopdelay, NULL);
 
         while (!thread_stop_requested) {
-                struct timespec sleep_spec;
+                curms_t now = curms();
 
-                now = curms();
-                if (active_threads == 0 && in_iteration) {
-                        curms_t to_sleep_ms = interval - now;
-                        if (verbosity > 0) {
-                                double elapsed = (statistics.query_threads_finished - iteration_started) / 1000.0;
-                                double to_sleep_s = to_sleep_ms / 1000.0;
-                                cllog(1, "Iteration #%d complete.", statistics.iterations);
-                                cllog(1, " %6.02f seconds elapsed", elapsed);
-                                cllog(1, " %6d SNMP queries made (%.01f queries/s)", statistics.snmp_success + statistics.snmp_fail, (statistics.snmp_success + statistics.snmp_fail) / elapsed);
-                                cllog(1, " %6d of those queries failed", statistics.snmp_fail);
-                                cllog(1, " %6d database inserts queued (%.01f queries/s)", statistics.insert_queries - statistics.dropped_queries, (statistics.insert_queries - statistics.dropped_queries) / elapsed);
-                                cllog(1, " %6d inserts were dropped due to lack of buffer space", statistics.dropped_queries);
-                                cllog(1, " %6d entries maximum queue size (%.01f %% full)", statistics.max_queue_depth, 100.0 * statistics.max_queue_depth / config->max_db_queue);
-                                if (to_sleep_ms > 0)
-                                        cllog(1, " %6.02f seconds until next iteration", to_sleep_s);
-                        }
-
-                        statistics.insert_rows = 0;
-                        statistics.insert_queries = 0;
-                        statistics.snmp_fail = 0;
-                        statistics.snmp_success = 0;
-                        statistics.dropped_queries = 0;
-                        statistics.max_queue_depth = 0;
-                        in_iteration = 0;
-
-                        if (to_sleep_ms > 0) {
-                                sleep_spec.tv_sec = (interval - now) / 1000;
-                                sleep_spec.tv_nsec = ((interval - now) % 1000) * 1000000;
-                                pthread_mutex_lock(&global_lock);
-                                pthread_cond_timedwait(&global_cond, &global_lock, &sleep_spec);
-                                pthread_mutex_unlock(&global_lock);
-                        }
+                /* If there are no threads running and it's time for a new iteration,
+                 * start a new iteration. */
+                if (active_threads == 0 && now >= next_iteration) {
+                        start_iteration(targets);
+                        this_iteration = now;
+                        next_iteration = next_interval(now, poll_interval);
                 }
 
-                now = curms();
-                if (active_threads == 0 && now >= interval) {
-                        unsigned qd;
-
-                        interval = 1000 * ((now / 1000) / poll_interval + 1) * poll_interval;
-                        in_iteration = 1;
-                        statistics.iterations++;
-
-                        cllog(1, "Starting iteration #%d.", statistics.iterations);
-                        qd = clbuf_count_used(queries);
-                        if (qd > 0)
-                                cllog(1, "  Queue at depth %d at poll start.", qd);
-
-                        pthread_mutex_lock(&global_lock);
-                        rtgtargets_reset_next(targets);
-                        pthread_cond_broadcast(&global_cond);
-                        pthread_mutex_unlock(&global_lock);
-                        iteration_started = curms();
-                }
-
+                /* Give the threads a chance to start, so we don't
+                 * mistakenly think they're all done already. */
                 nanosleep(&loopdelay, NULL);
+
+                /* If there are no threads running, and we have started a iteration,
+                 * then we are done. End the iteration. */
+                if (active_threads == 0 && this_iteration) {
+                        end_iteration(next_iteration, this_iteration);
+                        this_iteration = 0;
+                }
         }
+
         return NULL;
+}
+
+void start_iteration(struct rtgtargets *targets)
+{
+        statistics.iterations++;
+
+        if (verbosity > 0) {
+                unsigned qd = clbuf_count_used(queries);
+                cllog(1, "Starting iteration #%d.", statistics.iterations);
+                if (qd > 0)
+                        cllog(1, "  Queue at depth %d at poll start.", qd);
+        }
+
+        pthread_mutex_lock(&global_lock);
+        rtgtargets_reset_next(targets);
+        pthread_cond_broadcast(&global_cond);
+        pthread_mutex_unlock(&global_lock);
+}
+
+void end_iteration(curms_t next_iteration, curms_t this_iteration)
+{
+        curms_t now = curms();
+        curms_t to_sleep_ms = next_iteration - now;
+        if (verbosity > 0) {
+                double elapsed = (statistics.query_threads_finished - this_iteration) / 1000.0;
+                double to_sleep_s = to_sleep_ms / 1000.0;
+                cllog(1, "Iteration #%d complete.", statistics.iterations);
+                cllog(1, " %6.02f seconds elapsed", elapsed);
+                cllog(1, " %6d SNMP queries made (%.01f queries/s)", statistics.snmp_success + statistics.snmp_fail, (statistics.snmp_success + statistics.snmp_fail) / elapsed);
+                cllog(1, " %6d of those queries failed", statistics.snmp_fail);
+                cllog(1, " %6d database inserts queued (%.01f queries/s)", statistics.insert_queries - statistics.dropped_queries, (statistics.insert_queries - statistics.dropped_queries) / elapsed);
+                cllog(1, " %6d inserts were dropped due to lack of buffer space", statistics.dropped_queries);
+                cllog(1, " %6d entries maximum queue size", statistics.max_queue_depth);
+                if (to_sleep_ms > 0)
+                        cllog(1, " %6.02f seconds until next iteration", to_sleep_s);
+        }
+
+        statistics.insert_rows = 0;
+        statistics.insert_queries = 0;
+        statistics.snmp_fail = 0;
+        statistics.snmp_success = 0;
+        statistics.dropped_queries = 0;
+        statistics.max_queue_depth = 0;
+
+        if (to_sleep_ms > 0) {
+                struct timespec sleep_spec;
+                sleep_spec.tv_sec = (next_iteration - now) / 1000;
+                sleep_spec.tv_nsec = ((next_iteration - now) % 1000) * 1000000;
+
+                pthread_mutex_lock(&global_lock);
+                pthread_cond_timedwait(&global_cond, &global_lock, &sleep_spec);
+                pthread_mutex_unlock(&global_lock);
+        }
 }
